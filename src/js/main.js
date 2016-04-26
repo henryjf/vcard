5a01833df8065c0b44d33be53dfc8a472e30e016
JEFF HENRY <henry_jf@hotmail.com>
2016-04-26 15:00:42 -0400

    added token

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
 import $ from 'jquery';
 
+import token from './gihub-token';
+
+$.ajaxSetup({
+  headers: {
+    Authorization: "token " + token
+  }
+});
+
 var url = 'https://api.github.com/users/henryjf';
+
+$.getJSON(url, function (res) {
+  console.log(res);
+});
+
+// var card = function(info){
+//   return `
+//   <header>
+//     Jefferson Freeman Henry
+//   </header>
+//
+//   <div class="sidebar">
+//     <h3>The Basics</h3>
+//     <ul>
+//       <li>Name</li>
+//       <li>Github URL</li>
+//       <li>Email:</li>
+//       <li>Company:</li>
+//       <li>Website:</li>
+//     </ul>
+//   </div>`
+// };
+// console.log(card);
